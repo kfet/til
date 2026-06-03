23a9ef3bdf1408a7d033321732946c8d7d3f3181
Kalin Fetvadjiev <kfet@kfet.org>
2026-06-03 14:18:36 -0700

    Bump version to 1.1.0

--- til_cli/setup.py
+++ til_cli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="til-cli",
-    version="1.0.0",
+    version="1.1.0",
     description="TIL CLI Tool - Manage Today I Learned entries",
     long_description=open("../README.md").read(),
     long_description_content_type="text/markdown",
